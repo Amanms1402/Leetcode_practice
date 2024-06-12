15a592da93af1e48013b8510961f04c3209c5d57
Aman Kumar Singh <amanms1402@gmail.com>
2024-06-12 12:06:53 +0530

    Time: 27 ms (97.72%) | Memory: 16.3 MB (97.30%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        freq=[0]*3
+        for x in nums: freq[x]+=1
+        count=0
+        for x in range(3):
+            nums[count:count+freq[x]] = [x]*freq[x]
+            count+= freq[x]
+
+        
